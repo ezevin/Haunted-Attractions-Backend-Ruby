b83c388e4147f9a35b489f3887e9a3bdeccae34e
ezevin <ehzevin@gmail.com>
2019-12-20 14:57:28 -0500

    fixed cors error

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -4,5 +4,5 @@ class Attraction < ApplicationRecord
   validates :name, presence: true
   validates :attraction_type, inclusion: { in: %w( house castle cemetary bar/restaurant landmark ),
     message: "%{value} is not a valid type of attraction" }
-  validates :zip_code, length: { maximum: 5, minumum:  5, wrong_length: "Please enter a valid zip code" } }, numericality: true
+  validates :zip_code, length: { maximum: 5, minumum:  5, wrong_length: "Please enter a valid zip code" }, numericality: true
 end
